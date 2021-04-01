d907c68dd6fc9f63c667ec463c28a690879bd22f
Ayush Goyal <perfectayush@gmail.com>
2021-04-02 01:51:15 +0530

    Allow getting value arguments from environment variables

--- src/cli.rs
+++ src/cli.rs
@@ -9,12 +9,14 @@ pub fn get_cli_args() -> ArgMatches<'static> {
                 .short("t")
                 .long("token")
                 .value_name("API_TOKEN")
+                .env("LOGGLY_API_TOKEN")
                 .help("Loggly API token to use")
                 .takes_value(true))
         .arg(Arg::with_name("account")
              .short("a")
              .long("account")
              .value_name("ACCOUNT")
+             .env("LOGGLY_ACCOUNT")
              .help("Loggly Account to search logs in use")
              .takes_value(true)).get_matches();
     matches
